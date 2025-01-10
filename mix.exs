2d654e4461b24f29fa472e00f5097939443c6df4
José Victor Zeller <josevictorzg@gmail.com>
2025-01-09 22:26:44 -0300

    chore(build): add code quality and ci checks

--- mix.exs
+++ mix.exs
@@ -7,7 +7,20 @@ defmodule KubeProbex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_deps: :apps_direct,
+        plt_add_apps: [
+          :ex_unit,
+          :mix,
+          :plug
+        ],
+        list_unused_filters: true,
+        # we use the following opt to change the PLT path
+        # even though the opt is marked as deprecated, this is the doc-recommended way
+        # to do this
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -21,7 +34,12 @@ defmodule KubeProbex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug, "~> 1.16"}
+      # Phoenix Plug
+      {:plug, "~> 1.16"},
+
+      # Linters, formatters, typespecs, and docs
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 end
