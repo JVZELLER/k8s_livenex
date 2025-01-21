d4a1213822a554482b515ce2b71e75b4f4d59194
José Victor Zeller <josevictorzg@gmail.com>
2025-01-20 21:48:00 -0300

    docs: improve liveness plug documentation

--- lib/kube_probex/plug/liveness.ex
+++ lib/kube_probex/plug/liveness.ex
@@ -1,7 +1,36 @@
 if {:module, Plug} == Code.ensure_compiled(Plug) do
   defmodule KubeProbex.Plug.Liveness do
     @moduledoc """
-    Documentation for `KubeProbex`.
+    A plug for handling Kubernetes HTTP liveness probe requests.
+
+    This module integrates with Phoenix or Plug applications to define a liveness probe
+    endpoint. It validates the incoming request's path and executes the configured liveness
+    check logic.
+
+    ## Default Behavior
+
+    - The default path for liveness probes is `"/healthz"`.
+    - The liveness check is performed using the `KubeProbex.Check.Liveness` behaviour.
+    - If the incoming request's path does not match the expected path, the request is passed
+      through unaltered.
+
+    ## Configuration
+
+    You can customize the path for the liveness probe by providing a `:path` option
+    when configuring this plug. If no custom path is provided, the default path `"/healthz"`
+    will be used.
+
+    ## Example
+
+    Add the liveness plug to your router or endpoint to define the liveness probe:
+
+      ```elixir
+      defmodule MyAppWeb.Router do
+        use Phoenix.Endpoint, otp_app: :my_app_web
+
+        plug KubeProbex.Plug.Liveness, path: ~w(/_health /_healthz)
+      end
+      ```
     """
 
     @behaviour Plug
